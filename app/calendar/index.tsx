17fd780930058963a6a927e594e3786d7a0f5a33
Matthias Froehlich <froehlich_matthias@yahoo.com>
2024-12-21 20:01:18 +0100

    added rest of test data to db; created controller folder

--- app/calendar/index.tsx
+++ app/calendar/index.tsx
@@ -1,34 +1,57 @@
-import { View, Text, Pressable } from "react-native";
 import { useDatabase } from "@/context/DatabaseContext";
 import {
-    journals as journalTestData,
-    users as userTestData,
-} from "@/assets/testData";
-import { Journal, User } from "@/types/modelTypes";
-import { journalsTable, usersTable } from "@/db/schema";
+    Journal,
+    User,
+    Run,
+    ScheduledWorkout,
+    Equipment,
+    RunEquipmentMapping,
+} from "@/types/modelTypes";
+import {
+    journalsTable,
+    usersTable,
+    runsTable,
+    scheduledWorkoutsTable,
+    equipmentTable,
+    runsEquipmentTable,
+} from "@/db/schema";
+import { Button, Text, useTheme } from "react-native-paper";
+import { View } from "react-native";
+import { useLiveQuery } from "drizzle-orm/expo-sqlite";
+import { eq } from "drizzle-orm";
 
 const Calendar = () => {
-    const db = useDatabase();
-    console.log("db", db);
+    const { db, user } = useDatabase();
+    const theme = useTheme();
 
-    const insertTestData = async () => {
-        console.log("inserting test data");
+    if (!db) {
+        return <Text>Database not found</Text>;
+    }
 
-        // successfully inserted test data of users and journals
-        // await db.insert(usersTable).values(userTestData).onConflictDoNothing();
-        // await db
-        //     .insert(journalsTable)
-        //     .values(journalTestData)
-        //     .onConflictDoNothing();
-        console.log("test data inserted");
-    };
+    if (!user) {
+        return <Text>User not found</Text>;
+    }
+
+    // ToDo: get journal by date of maybe we integrate a setCurrent setting saved in local storage
+    // and fetch the latest journal entry by default as long setCurrent is not set
+    const { data: journals } = useLiveQuery(
+        db.select().from(journalsTable).where(eq(journalsTable.userId, user.id))
+    );
+
+    const firstJournalEntry: Journal = journals[0];
+
+    console.log("data", journals);
 
     return (
-        <View>
-            <Text>Calendar</Text>
-            <Pressable onPress={() => insertTestData()}>
-                <Text>Press me</Text>
-            </Pressable>
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "flex-start",
+                alignItems: "center",
+                backgroundColor: theme.colors.background,
+            }}
+        >
+            <Text>{firstJournalEntry.title}</Text>
         </View>
     );
 };
